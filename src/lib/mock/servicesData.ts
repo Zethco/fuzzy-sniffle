33418915968daeca4e78e721a91ad4b51a044ea4
andreslenis <andreslenis01@gmail.com>
2024-02-24 23:06:55 -0500

    feature: add hardcoded text to landing page

--- src/lib/mock/servicesData.ts
+++ src/lib/mock/servicesData.ts
@@ -1,37 +1,37 @@
 export const services = [
   {
-      title: "Social media marketing",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+      title: "Asesoria Software",
+      description: "Asesoría y analisis para el mejoramiento de sistemas a nivel de software",
       icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
       </svg>`,
   },
   {
-      title: "Amazon affiliate marketing",
-      description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
+      title: "Desarrollo de Soluciones WEB / APP",
+      description: "Desarrollos de soluciones a demanda y bajo petición segun las necesidades",
       icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
       </svg>`,
   },
   {
-      title: "Email marketing",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+      title: "Contratación",
+      description: "Atendemos sus solicitudes de talento y evaluamos a las mejores opciones",
       icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
       </svg>`,
   },
   {
-      title: "Social media marketing",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+      title: "Asesoria de Procesos",
+      description: "Asesoría sobre tecnificación de procesos manuales",
       icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
       </svg>`,
   },
   {
-      title: "Amazon affiliate marketing",
-      description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
+      title: "Fotografia Profesional",
+      description: "Asesoría y toma de fotografía a cualquier nivel (Profesional, Semi Profesional, Amateur)",
       icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
       </svg>`,
   },
   {
-      title: "Email marketing",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+      title: "Manejo de Redes Sociales",
+      description: "Asesoría y manejo de redes sociales (Instagram, Facebook, TikTok, Likedin)",
       icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
       </svg>`,
   },
