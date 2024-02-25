7cedd8d9c9a9a954044c34a9ac34f5819a130531
Santiago Arias <santiago.arias@qrvey.com>
2024-02-25 00:23:18 -0500

    ci: fix some vercel things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [react(), tailwind()],
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
